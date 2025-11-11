cbbeff32b03eb3f4883d3284eafec8b8102cdb32
Crfoli <crfoli@yahoo.com.br>
2025-11-10 22:06:02 -0300

    Remove diskcache (não é mais necessário com Postgres)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# app.py (CORRIGIDO: Usando background_callback_manager)
+# app.py (CORRIGIDO E LIMPO - Sem diskcache)
 
 import dash
 import dash_bootstrap_components as dbc
@@ -24,31 +24,11 @@ META_TAGS = [
      "content": "width=1200, initial-scale=0.25, minimum-scale=0.1, user-scalable=yes"}
 ]
 
-# --- INÍCIO DA SOLUÇÃO DO ERRO DE BACKGROUND ---
-from dash import DiskcacheManager as DiskcacheLongCallbackManager
-import diskcache
-import os
-
-# Define a pasta onde o cache será armazenado
-CACHE_DIR = os.path.join(os.getcwd(), "cache_callbacks")
-if not os.path.exists(CACHE_DIR):
-    os.makedirs(CACHE_DIR)
-
-cache = diskcache.Cache(CACHE_DIR)
-# O DiskcacheLongCallbackManager agora é importado como DiskcacheManager
-long_callback_manager = DiskcacheLongCallbackManager(cache)
-# --- FIM DA SOLUÇÃO ---
-
-
 app = dash.Dash(__name__,
                 external_stylesheets=[THEME] + LEAFLET_CSS + MEU_CSS_LOCAL,
                 meta_tags=META_TAGS,
                 suppress_callback_exceptions=True,
-
-                # --- APLICA O GERENCIADOR DE CALLBACKS LONGOS ---
-                # ** Linha Corrigida: Renomeado para background_callback_manager **
-                background_callback_manager=long_callback_manager
-                # -----------------------------------------------
+                # Não há mais 'background_callback_manager'
                 )
 
 app.title = "Monitoramento Geoambiental"
